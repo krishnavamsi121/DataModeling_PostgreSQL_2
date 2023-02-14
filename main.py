6b4413a8ae1edc1664b59d91286f14fe4a12412b
Krishna <vamsirk99@gmail.com>
2023-02-13 18:43:48 -0600

    database connection established

---
+++ main.py
@@ -0,0 +1,26 @@
+import psycopg2
+from config import config
+
+def connect():
+    conn = None
+    try:
+        params = config()
+
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute('Select version()')
+
+        db_version = cur.fetchone()
+        print(db_version)
+
+        conn.close()
+
+    except (Exception,psycopg2.DatabaseError) as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed')
+
+if __name__ == "__main__":
+    connect()
